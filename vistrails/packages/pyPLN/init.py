1f8eeefd7bdd6d4fa5e8e29866d3ddbe488657db
bferrazAC <brunoferraz24@gmail.com>
2018-04-27 00:55:19 -0300

    Add module LoadNLTKCorpus

--- vistrails/packages/pyPLN/init.py
+++ vistrails/packages/pyPLN/init.py
@@ -4,18 +4,39 @@ from vistrails.core.modules.config import IPort, OPort
 
 import os
 
-# Module to type the Corpus
+############## Comment Pattern ##############
+
+# Module:
 # Parameters:
-# Return
+# Return:
+
+	# Method:
+	# Parameters:
+	# Return:
+
+##############
+
+############## Workflow Types ###############
+
+# Module to create Corpus as parameterer's type
+# Parameters: (Directory) A directory to load the corpus from 
+# Return:
 class Corpus(Module):
 	corpus_dir = None
 	corpus = None
-	def __init__(self, corpus_dir):
+	def __init__(self, corpus_obj = None, corpus_dir = None):
 		self.corpus_dir = corpus_dir
-	
-		self.corpus = self.load_corpus_dir()
+		
+		if ( corpus_dir != None):
+			self.corpus = self.load_corpus_dir()
+
+		if corpus_obj != None:
+			self.corpus = corpus_obj
 		# print (check_corpus())
 
+	# Method to load the corpus 
+	# Parameters:
+	# Return: The Nltk.Corpus structure 
 	def load_corpus_dir(self):
 			from nltk.corpus import BracketParseCorpusReader
 
@@ -30,6 +51,9 @@ class Corpus(Module):
 			
 			return corpus
 
+	# Method to check the corpus (debug)
+	# Parameters:
+	# Return: 1 for existing corpus, 0 for not
 	def check_corpus(self):
 		files_ids = self.corpus.fileids()
 
@@ -39,6 +63,10 @@ class Corpus(Module):
 
 		return 0
 
+
+# Module to create Tokens as parameters' type
+# Parameters: (String) A raw text to tokenize 
+# Return:
 class Tokens(Module):
 	raw = None
 	tok_sen = None
@@ -47,12 +75,19 @@ class Tokens(Module):
 		self.raw = raw
 		self.tok_sen = self.tokenize(raw)
 
+	# Method to tokenize a text
+	# Parameters: (String) Text
+	# Return:
 	def tokenize(self, raw):
 		from nltk import word_tokenize
 
 		tok_sen = word_tokenize(raw)
 
 		return tok_sen
+
+##############
+
+############## Workflow Modules ##############
 
 # Module set up the content of ntlk to user's acess
 # Parameters: None
@@ -83,24 +118,45 @@ class ShowNLTKCorpus(Module):
 		
 		corpuses_file = open(abs_file_path,'r')
 		
-		corpuses_list = []
-		for corpus in corpuses_file:
-			corpuses_list.append(corpus)
+		corpuses_list = [corpus.strip('\n') for corpus in corpuses_file]
+
 		print "There are this corpuses available from Nltk Package"
 		print " " + str(corpuses_list) + " "
 		self.set_output('output_corpus', corpuses_list)
 
+# Module loads a corpus object by a specified corpus in NLTK
+# Parameters: (string) corpus name
+# Return: (Corpus) corpus object
+class LoadNLTKCorpus(Module):
+	_input_ports = [IPort(name = "input_corpus", signature = "basic:String")]
+	_output_ports = [OPort(name = "output_corpus_object", signature = "Corpus")]
+
+	def compute(self):
+		corpus_str = self.get_input('input_corpus')
+
+		corpus = self.get_corpus(corpus_str)
+
+		self.set_output('output_corpus_object',corpus)
+
+	def get_corpus(self, corpus_str):
+		import nltk.corpus as ncp
+
+		corpus = getattr(ncp,corpus_str)
+		corpus_obj = Corpus(corpus_obj = corpus)
+
+		return corpus_obj 
+
 # Module load the user's provided corpus
 # Parameters:
 # Return
-class LoadCorpus(Module):
+class LoadMyCorpus(Module):
 		
 		_input_ports = [IPort(name = "input_corpus", signature = "basic:Directory")]
 		_output_ports = [OPort(name = "output_corpus", signature = "Corpus")]
 		def compute(self):
 			
 			corpus_dir = self.get_input('input_corpus')
-			corpus = Corpus(corpus_dir)
+			corpus = Corpus(corpus_dir = corpus_dir)
 			# corpus.check_corpus()
 			self.set_output('output_corpus', corpus)
 
@@ -117,11 +173,9 @@ class Tokenizer(Module):
 
 		self.set_output('output_tokens', tokObject)
 
+##############
 
 
-# Module
-# Parameters:
-# Return
 
 
-_modules = [UpdateNltkCorpus, ShowNLTKCorpus,LoadCorpus, Corpus, Tokens, Tokenizer]
+_modules = [UpdateNltkCorpus, ShowNLTKCorpus,LoadMyCorpus, LoadNLTKCorpus, Corpus, Tokens, Tokenizer]
